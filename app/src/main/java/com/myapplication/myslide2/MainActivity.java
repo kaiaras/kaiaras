d6b83479cd99eb13f9008cc99bfd2b9856d42aaf
kaiaras <kaiaras@nate.com>
2015-09-30 16:03:48 +0900

    my commit message

--- app/src/main/java/com/myapplication/myslide2/MainActivity.java
+++ app/src/main/java/com/myapplication/myslide2/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TabHost;
 
 import java.util.Locale;
 
@@ -24,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost = new TabHost();
+        tabHost = (MaterialTabHost)
 
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
